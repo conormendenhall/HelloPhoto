f52edeb54485b958d7ee288e5a3aaf397588e2c5
Matt Mitchell <mjmitchell86@gmail.com>
2017-09-15 11:51:31 -0400

    Add S3 shtuff to Photo Repo (and aws sdk dependencies to app).  Must have credentials file saved on system to run.

--- HelloPhoto/Repositories/PhotoRepository.cs
+++ HelloPhoto/Repositories/PhotoRepository.cs
@@ -4,6 +4,8 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace HelloPhoto
 {
@@ -11,18 +13,28 @@ namespace HelloPhoto
 	{
 		public async Task<Photo> Save(Photo photo)
 		{
-			var client = new HttpClient();
-			var stringContent = new StringContent(JsonConvert.SerializeObject(photo), Encoding.UTF8, "application/json");
+            //var client = new HttpClient();
+            //var stringContent = new StringContent(JsonConvert.SerializeObject(photo), Encoding.UTF8, "application/json");
 
-			var result =
-				await client.PostAsync("http://hellophotoapi-prod.us-east-1.elasticbeanstalk.com/api/Photos", stringContent);
+            //var result =
+            //	await client.PostAsync("http://hellophotoapi-prod.us-east-1.elasticbeanstalk.com/api/Photos", stringContent);
 
-			if (result.StatusCode == HttpStatusCode.OK)
-			{
-				return photo;
-			}
+            //if (result.StatusCode == HttpStatusCode.OK)
+            //{
+            //	return photo;
+            //}
 
-			return null;
+            IAmazonS3 client;
+            client = new AmazonS3Client(Amazon.RegionEndpoint.USEast2);
+            PutObjectRequest request = new PutObjectRequest()
+            {
+                BucketName = "hellophoto-det",
+                Key = photo.PhotoId.ToString(),
+                FilePath = //FilePath
+            };
+            PutObjectResponse response2 = await client.PutObjectAsync(request);
+
+            return null;
 		}
 	}
 }
